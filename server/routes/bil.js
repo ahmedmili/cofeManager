66cf5625e136abace177d6b2ffb5cd7ee792b573
ahmedmili <ahmedmili76@gmail.com>
2022-11-16 00:43:52 +0100

    [UPDATE] new bill routes added

--- server/routes/bil.js
+++ server/routes/bil.js
@@ -45,7 +45,6 @@ router.post('/generateReport',auth.authToken,(req,res)=>{
                 }else{
                     pdf.create(results).toFile('./generated_pdf/'+generateuuid+'.pdf',function(err,data){
                         if(err){
-                            console.log(err);
                             return res.status(500).json(err);
                         }else{
                             return res.status(200).json({uuid:generateuuid})
@@ -61,8 +60,72 @@ router.post('/generateReport',auth.authToken,(req,res)=>{
 })
 
 
-router.post
+router.post("/getPdf",auth.authToken,(req,res)=>{
+    const orderDetails = req.body;
+    const pdfPath = "./generated_pdf/"+orderDetails.uuid+".pdf";
+    if(fs.existsSync(pdfPath)){
+        res.contentType("application/pdf");
+        fs.createReadStream(pdfPath).pipe(res);
+    }else{
+        var productDetailsReport = JSON.parse(JSON.stringify( orderDetails.productDetails))
+       
+        ejs.renderFile(path.join(__dirname,"report.ejs"),{
+            productDetails:productDetailsReport,
+            name:orderDetails.name,
+            email:orderDetails.email,
+            contactNumber:orderDetails.contactNumber,
+            paymentMethod:orderDetails.paymentMethod,
+            totalAmount:orderDetails.totalAmount
+        },function(err,results){
+            if(err){
+                // console.log("err")
+                return res.status(500).json(err);
+            }else{
+                pdf.create(results).toFile(pdfPath,function(err,data){
+                    if(err){
+                        // console.log("err");
+                        return res.status(500).json(err);
+                    }else{
+                        res.contentType("application/pdf");
+                        fs.createReadStream(pdfPath).pipe(res);           
+                     }
+                })
+            }
+        })
+    
+    }
+})
+
+router.get("/getBills",auth.authToken,(req,res)=>{
+    var query = "select * from bille order by id DESC"
+    connection.query(query,(err,results)=>{
+        if(!err){
+            return res.status(200).json(results)
+        }else{
+            return res.status(500).json(err); 
+        }
+    })
+})  
+
+router.delete("/delete/:id",auth.authToken,(req,res)=>{
+    const id = req.params.id
+    var query = "delete from bille where id=?"
+    connection.query(query,[id],(err,results)=>{
+        if(!err){
+            if(results.affectedRows == 0){
+                return res.status(404).json({msg:"bill id does not found"})
+            }
+            return res.status(200).json({msg:"Bill Deleted Successfully"})
+        }else{
+            return res.status(500).json(err); 
+        }
+    })
+})
 
 
 
-module.exports 
+
+
+module.exports = router;
+
+
